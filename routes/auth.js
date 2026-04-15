e1264791c389453b1ff1d4092c6341c5dc65570c
Lorenzo Lorusso <lorusso.lorenzo98@gmail.com>
2026-04-15 21:25:58 +0200

    fix 3 per deploy

--- routes/auth.js
+++ routes/auth.js
@@ -4,20 +4,21 @@ import { authentication } from "../middlewares/authentication.js"
 import passport from "passport"
 
 const authRouter = express.Router()
-//definiamo le rotte
+
 authRouter.post('/login', login)
 authRouter.get('/me', authentication, me)
+
 authRouter.get(
     "/google",
     passport.authenticate("google", { scope: ["profile", "email"] })
 );
+
 authRouter.get(
     "/google/callback",
     passport.authenticate("google", { session: false }),
     (req, res) => {
-        res.redirect(`http://localhost:3001/login?token=${req.user.token}`);
+        res.redirect(`${process.env.FRONTEND_URL}/login?token=${req.user.token}`);
     }
 );
-
 
 export default authRouter
